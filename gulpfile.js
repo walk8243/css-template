1e6ae820a31a6c39c2be7df12a95bd6f23e817fd
walk8243 <walk8243@gmail.com>
2018-06-17 17:16:20 +0900

    autoprefixとガイド作成のモジュール追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp  = require('gulp'),
-    sass  = require('gulp-sass');
+    sass  = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    frontnote = require('gulp-frontnote');
 
 // タスクの定義
 gulp.task('sass', function() {
   gulp.src('./sass/**/*.scss')
+    .pipe(frontnote({
+      css: '../css/style.css'
+    }))
     .pipe(
       sass({
         outputStyle: 'compressed'
       }).on('error', sass.logError)
-    ).pipe(gulp.dest('./css'));
+    )
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'));
 });
 
 // デフォルトでの起動時の設定
