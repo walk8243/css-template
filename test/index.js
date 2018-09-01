1a37edc3e9f3f5b856cde16fe804b961693b6137
walk8243 <walk8243@gmail.com>
2018-09-02 00:25:46 +0900

    setup test

---
+++ test/index.js
@@ -0,0 +1,10 @@
+const assert  = require('assert'),
+      sinon   = require('sinon');
+const cssTemplate = require('../index'),
+      func        = require('../modules/func');
+
+describe('Module script', () => {
+  it('関数の存在確認', () => {
+    console.log(cssTemplate);
+  });
+});
