dbdfd845a868508d924781a07805acc43bbbf8a2
walk8243 <walk8243@gmail.com>
2018-09-01 22:42:56 +0900

    不要なコードを削除

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const CleanCSS  = require('clean-css'),
       sass      = require('node-sass');
 
 module.exports = (src = './sass/style.scss', dest = './public/style.css') => {
-  var data = '@import "import";\n\n' + fs.readFileSync(src);
-  // console.log(data);
   return new Promise((resolve, reject) => {
     sass.render({
-      data: data,
+      data: '@import "import";\n\n' + fs.readFileSync(src),
       includePaths: [`${__dirname}/sass`],
       outputStyle: 'expanded'
     }, (error, result) => {
@@ -15,7 +13,6 @@ module.exports = (src = './sass/style.scss', dest = './public/style.css') => {
       var minimized = new CleanCSS().minify(result.css.toString()).styles;
       fs.writeFile(dest, minimized, err => {
         if(err) return reject(err);
-        // console.log('The file has been saved!');
         resolve();
       });
     });
