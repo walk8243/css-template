f819c6a63526ea0f72487186f2883daafa1babdd
walk8243 <walk8243@gmail.com>
2018-09-01 22:22:48 +0900

    clean-cssを使用してcssを圧縮

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const fs = require('fs'),
-      sass = require('node-sass');
+const CleanCSS  = require('clean-css'),
+      fs        = require('fs'),
+      sass      = require('node-sass');
 
 module.exports = (src = './sass/style.scss', dest = './public/style.css') => {
   var data = '@import "import";\n\n' + fs.readFileSync(src);
@@ -7,10 +8,12 @@ module.exports = (src = './sass/style.scss', dest = './public/style.css') => {
   return new Promise((resolve, reject) => {
     sass.render({
       data: data,
-      includePaths: [`${__dirname}/sass`]
+      includePaths: [`${__dirname}/sass`],
+      outputStyle: 'expanded'
     }, (error, result) => {
       if(error) return reject(error);
-      fs.writeFile(dest, result.css.toString(), err => {
+      var minimized = new CleanCSS().minify(result.css.toString()).styles;
+      fs.writeFile(dest, minimized, err => {
         if(err) return reject(err);
         // console.log('The file has been saved!');
         resolve();
