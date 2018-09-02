cb84b11ec60451e1914a843aa4eca897f1acbd67
walk8243 <walk8243@gmail.com>
2018-09-02 12:20:48 +0900

    コンパイルと書き込み部分をモジュール化

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-const CleanCSS  = require('clean-css'),
-      fs        = require('fs'),
-      sass      = require('node-sass');
-const func      = require('./modules/func');
+const sass      = require('./modules/sass');
 
 
-module.exports = (src = './sass/style.scss', dest = './public/style.css') => {
-  return new Promise((resolve, reject) => {
-    if(!func.isExistFile(src)) return reject(new Error(`'${src}'が存在しないか、ファイルではありません。`));
-    sass.render({
-      data: '@import "import";\n\n' + fs.readFileSync(src),
-      includePaths: [`${__dirname}/sass`],
-      outputStyle: 'expanded'
-    }, (error, result) => {
-      if(error) return reject(error);
-      func.prepareStorage(dest);
-      var minimized = new CleanCSS().minify(result.css.toString()).styles;
-      fs.writeFile(dest, minimized, err => {
-        if(err) return reject(err);
-        resolve();
-      });
-    });
-  });
+module.exports = async (src = './sass/style.scss', dest = './public/style.css') => {
+  try {
+    var result = await sass.render(src);
+    await sass.writeFile(result.css.toString(), dest);
+    return;
+  } catch(error) {
+    throw error;
+  }
 };
