ffa8cbacd6c8bc77042b771d76c5464a6aa4e52b
walk8243 <walk8243@gmail.com>
2018-09-02 20:03:24 +0900

    ディレクトリの存在チェックのテスト実装

--- test/modules.func.js
+++ test/modules.func.js
@@ -4,7 +4,7 @@ const assert    = require('assert'),
       sinon     = require('sinon');
 const func      = require('../modules/func');
 
-describe.only('func Module', () => {
+describe('func Module', () => {
   it('Moduleの内容確認', () => {
     assert.deepEqual(Object.keys(func), ['isExistFile', 'isExistDir', 'prepareStorage']);
     assert.equal(typeof func.isExistFile, 'function');
@@ -50,6 +50,48 @@ describe.only('func Module', () => {
         assert.ok(!func.isExistFile('fuga'));
         assert.ok(stubFsStatSync.calledOnce);
         assert.ok(stubStatsIsFile.calledOnce);
+      });
+    });
+  });
+
+  describe('isExistDir', () => {
+    var stubFsStatSync,
+        stubStatsIsDirectory;
+    const stats = new fs.Stats();
+    before(() => {
+      stubFsStatSync = sinon.stub(fs, 'statSync');
+      stubStatsIsDirectory = sinon.stub(stats, 'isDirectory');
+    });
+    after(() => {
+      stubFsStatSync.restore();
+      stubStatsIsDirectory.restore();
+    });
+    beforeEach(() => {
+      stubFsStatSync.returns(stats);
+      stubStatsIsDirectory.returns(true);
+    });
+    afterEach(() => {
+      stubFsStatSync.reset();
+      stubStatsIsDirectory.reset();
+    });
+
+    it('正常系', () => {
+      assert.ok(func.isExistDir('fuga'));
+      assert.ok(stubFsStatSync.calledOnce);
+      assert.ok(stubStatsIsDirectory.calledOnce);
+    });
+    describe('異常系', () => {
+      it('statSync Error', () => {
+        stubFsStatSync.throws();
+        assert.ok(!func.isExistDir('fuga'));
+        assert.ok(stubFsStatSync.calledOnce);
+        assert.ok(stubStatsIsDirectory.notCalled);
+      });
+      it('stats isDirectory Error', () => {
+        stubStatsIsDirectory.returns(false);
+        assert.ok(!func.isExistDir('fuga'));
+        assert.ok(stubFsStatSync.calledOnce);
+        assert.ok(stubStatsIsDirectory.calledOnce);
       });
     });
   });
