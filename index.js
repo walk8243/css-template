486d7feba53850b3be726d8918bf6f292a9e1ed0
walk8243 <walk8243@gmail.com>
2021-10-30 14:49:01 +0900

    node-sassからDart Sassに移行

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const sass      = require('./modules/sass');
 
-
 module.exports = async (src = './sass/style.scss', dest = './public/style.css') => {
   try {
     var result = await sass.render(src);
